cab6e10e34cb14eb8ffda1d1abf867c4b0310a6e
OlegPC <oleg.kovalenko@a-bank.com.ua>
2021-03-03 19:18:31 +0200

    new project

---
+++ src/main/java/ua/restoraunt/RestorauntApplication.java
@@ -0,0 +1,13 @@
+package ua.restoraunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorauntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestorauntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/restoraunt/RestorauntApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.restoraunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorauntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
